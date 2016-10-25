9049894b6ce28dca1979789db27dcad9a86bb350
undercloud <alexandrostruses@yandex.ru>
2016-10-25 13:59:05 +0400

    Update ReservoirTest.php

--- tests/Reservoir/Tests/ReservoirTest.php
+++ tests/Reservoir/Tests/ReservoirTest.php
@@ -163,6 +163,8 @@ class ReservoirTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(true, $baz->getFoo() instanceof \Foo);
         $this->assertEquals(true, $this->di->make([$baz,'quux']) instanceof \Quux);
         $this->assertEquals(true, $this->di->make('Baz::quux') instanceof \Quux);
+        $this->assertEquals(true, $this->di->make([$baz,'staticQuux']) instanceof \Quux);
+        $this->assertEquals(true, $this->di->make('Baz::staticQuux') instanceof \Quux);
         $this->assertEquals(true, $this->di->make($fn) instanceof \Foo);
 
         $foo = new \Foo;
